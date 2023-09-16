8b994c578e59c612b09df10a9adb11aa2997b62f
YksRocks <73733249+YksRocks@users.noreply.github.com>
2023-09-16 17:49:39 +0530

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
   container: "map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v11",
-  center: [-103.5917, 40.6699],
+  center: [80.321759, 26.460914],
   zoom: 3,
 });
 
