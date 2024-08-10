3932c04b789e46c8ea72d0aa847a80d9b532b064
Yash Kumar Singh <73733249+YksRocks@users.noreply.github.com>
2024-08-11 02:15:18 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const express = require("express");
