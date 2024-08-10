23c864c760b390343d43206c92140cebb03df12f
Yash Kumar Singh <73733249+YksRocks@users.noreply.github.com>
2024-08-11 02:25:41 +0530

    Update CatchAsync.js

--- utils/CatchAsync.js
+++ utils/CatchAsync.js
@@ -1,5 +1,8 @@
 module.exports = (func) => {
   return (req, res, next) => {
-    func(req, res, next).catch((e) => next(e));
+    func(req, res, next).catch((e) =>{
+      console.log(e); 
+      next(e);
+    });
   };
 };
