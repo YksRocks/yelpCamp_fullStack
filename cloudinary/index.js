c8ab5384849e77a772d0b80609b68655e1bf28a2
Yash Kumar Singh <73733249+YksRocks@users.noreply.github.com>
2024-08-11 02:19:42 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -7,9 +7,6 @@ const express = require("express");
 const multer = require("multer");
 const app = express();
 
-console.log(process.env.CLOUDINARY_CLOUD_NAME)
-console.log(process.env.CLOUDINARY_KEY)
-console.log(process.env.CLOUDINARY_SECRET)
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
